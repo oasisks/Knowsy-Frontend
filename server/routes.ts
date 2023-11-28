2ce9295da81d26015dc9645c9a441dfa2726515d
Hao Zhu <oasisks2@gmail.com>
2023-11-28 16:45:47 -0500

    Forgot to change the routes too

--- server/routes.ts
+++ server/routes.ts
@@ -2,7 +2,7 @@ import { ObjectId } from "mongodb";
 
 import { Router, getExpressRouter } from "./framework/router";
 
-import { Post, RadiusResource, User, WebSession } from "./app";
+import { LocationResource, Opinion, Post, RadiusResource, User, WebSession } from "./app";
 import { PostDoc, PostOptions } from "./concepts/post";
 import { UserDoc } from "./concepts/user";
 import { WebSessionDoc } from "./concepts/websession";
@@ -70,9 +70,9 @@ class Routes {
   }
 
   @Router.post("/posts")
-  async createPost(session: WebSessionDoc, content: string, options?: PostOptions) {
+  async createPost(session: WebSessionDoc, project: ObjectId, content: string, options?: PostOptions) {
     const user = WebSession.getUser(session);
-    const created = await Post.create(user, content, options);
+    const created = await Post.create(user, project, content, options);
     return { msg: created.msg, post: await Responses.post(created.post) };
   }
 
@@ -157,6 +157,77 @@ class Routes {
     });
 
     return await RadiusResource.changeCriticalDate(_id, _criticalDates);
+  }
+
+  @Router.post("/locationResources")
+  async createLocationResource(name: string, description: string, start: Date, status: string, longitude: number, latitude: number) {
+    const locationResource = await LocationResource.create(name, description, start, status, longitude, latitude);
+    
+    return;
+  }
+
+  @Router.get("/locationResources")
+  async getLocationResource(longitude: number, latitude: number, radius: number) {
+    const locationResources = await LocationResource.getWithinRadius(longitude, latitude, radius);
+    return locationResources;
+  }
+
+  @Router.get("/locationResources/:_id")
+  async getLocationResourceById(_id: ObjectId) {
+    const locationResource = await LocationResource.getById(_id);
+    return locationResource;
+  }
+
+  @Router.delete("/locationResources/:_id")
+  async deleteLocationResource(_id: ObjectId) {
+    const msg = await LocationResource.delete(_id);
+    return msg;
+  }
+
+  @Router.patch("/locationResources/:_id/location")
+  async updateLocationResourceLocation(_id: ObjectId, longitude: number, latitude: number) {
+    const msg = await LocationResource.update(_id, {location: { type: "Point", coordinates: [longitude, latitude] }});
+    return msg;
+  }
+
+  @Router.patch("/locationResources/:_id/status")
+  async updateLocationResourceStatus(_id: ObjectId, status: string) {
+    const msg = await LocationResource.update(_id, {status});
+    return msg;
+  }
+
+  @Router.patch("/locationResources/:_id/description")
+  async updateLocationResourceDescription(_id: ObjectId, description: string) {
+    const msg = await LocationResource.update(_id, {description});
+    return msg;
+  }
+
+
+
+
+  @Router.post("/posts/:_id/opinions")
+  async createOpinion(author: ObjectId, content: string, feeling: Number, root: ObjectId) {
+    return await Opinion.createOpinion(author, content, feeling, root);
+  }
+
+  @Router.get("/opinions")
+  async getOpinions(author?: ObjectId, root?: ObjectId) {
+    let opinions;
+    if (author) {
+      opinions = await Opinion.getOpinions(author);
+    } else if (root) {
+      opinions = await Opinion.getOpinions(root);
+    } else {
+      opinions = await Post.getPosts({});
+    }
+    return opinions;
+  }
+
+  @Router.delete("/opinions/:_id")
+  async deleteOpinion(session: WebSessionDoc, _id: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Opinion.isAuthor(user, _id);
+    return Opinion.delete(_id);
   }
 }
 
