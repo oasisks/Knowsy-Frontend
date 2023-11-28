1c108f17a2fb5f9fc070400bb5ccdde55925f51a
Hao Zhu <oasisks2@gmail.com>
2023-11-28 16:57:08 -0500

    Fixed a fatal error for google maps api

--- client/main.ts
+++ client/main.ts
@@ -1,11 +1,11 @@
 import "@/assets/main.css";
 import "purecss";
 
-import VueGoogleMaps from "@fawmi/vue-google-maps";
 import { createPinia } from "pinia";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
 
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 import App from "./App.vue";
 import router from "./router";
 
@@ -17,7 +17,7 @@ app.use(pinia);
 app.use(router);
 app.use(VueGoogleMaps, {
   load: {
-    key: process.env.GOOGLE_API,
+    key: "YOUR_API_KEY_COMES_HERE",
   },
 });
 
