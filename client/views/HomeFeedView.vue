f08fb5bcc461184c4a40438780c9c14d541cf1c5
Julia Camacho <juliac123@gmail.com>
2023-11-29 13:46:53 -0500

    add feed

---
+++ client/views/HomeFeedView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import FeedComponent from "@/components/FeedComponent.vue";
+import { useUserStore } from "@/stores/user";
+import { storeToRefs } from "pinia";
+
+const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
+</script>
+
+<template>
+  <main>
+    <h1>Home Feed</h1>
+    <section>
+      <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
+      <h1 v-else>Please login!</h1>
+    </section>
+    <FeedComponent />
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
