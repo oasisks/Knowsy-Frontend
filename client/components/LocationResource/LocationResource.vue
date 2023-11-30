ca1b6cb46cebbd4886222cf14a392b033aadceac
Hao Zhu <oasisks2@gmail.com>
2023-11-29 23:17:29 -0500

    Finished the map for locationResources

--- client/components/LocationResource/LocationResource.vue
+++ client/components/LocationResource/LocationResource.vue
@@ -9,12 +9,14 @@ import LocationPanel from "./LocationPanel.vue";
 const { currentUsername, isLoggedIn, userCoords } = storeToRefs(useUserStore());
 const center = ref({lat: 0, lng: 0});
 const ready = ref(false);
+const currentMarkerId = ref(-1);
 const options = ref({
-  styles: [
-    // this is for specific styles we might want to have on the map
-    // TODO: https://console.cloud.google.com/google/maps-apis/studio/styles?project=knowsy-406521
-    // Use the above link to create styles for our map
-  ]
+  mapId: 'eeb313ac772d983a'
+  // styles: [
+  //   // this is for specific styles we might want to have on the map
+  //   // TODO: https://console.cloud.google.com/google/maps-apis/studio/styles?project=knowsy-406521
+  //   // Use the above link to create styles for our map
+  // ]
 });
 
 const MITPOS = {lat: 42.3601, lng: 71.0942};
@@ -22,8 +24,7 @@ const MITPOS = {lat: 42.3601, lng: 71.0942};
 // we either get it from the user or we set the default location to be MIT
 center.value = isNaN(userCoords.value.position.lat) || isNaN(userCoords.value.position.lng) ? MITPOS : userCoords.value.position;
 
-const projects = ref([{position: center.value, title: "Home Location", status: "NAN", description: "It is my home :)"}]);
-const markers = ref([{position: center.value}]);
+const projects = ref([{id: -1, position: center.value, title: "Home Location", status: "NAN", description: "It is my home :)", home: true}]);
 
 // populate the map with markers
 async function populateMarkers() {
@@ -35,19 +36,25 @@ async function populateMarkers() {
   try {
     const locationResources = await fetchy('/api/locationResources', "GET", {query});
     console.log(locationResources);
+    let id = 0;
     locationResources.forEach((locationResource: any) => {
       console.log(locationResource);
       // for each project, add in the project title, content, and critical dates
       const position = locationResource.location.coordinates;
       const description = locationResource.description;
-      const title = locationResource.title;
+      const title = locationResource.name;
       const status = locationResource.status;
-      projects.value.push({position: {lng: position[0], lat: position[1]}, title, status, description});
-      markers.value.push({position: { lng: position[0], lat: position[1]}})
+      projects.value.push({id, position: {lng: position[0], lat: position[1]}, title, status, description, home: false});
+      id++;
     })
   } catch {
 
   }
+}
+
+function openMarker(id: number) {
+  currentMarkerId.value = id;
+  console.log(currentMarkerId.value);
 }
 
 onMounted(async () => {
@@ -59,22 +66,30 @@ onMounted(async () => {
   <GMapMap
       :center="center"
       :zoom="14"
+      :options="options"
       style="height: 89vh; width: 100%; display: flex;"
       >
       <GMapMarker
       :key="index"
       v-for="(project, index) in projects"
       :position="project.position"
+      :clickable="true"
+      @click="openMarker(project.id)"
+      @closeclick="openMarker(-1)"
       >
-      <GMapInfoWindow>
-        <div>
-          <p>dasd</p>
-          <LocationPanel />
-        </div>
-      </GMapInfoWindow>
+      <LocationPanel 
+        :title="project.title" 
+        :description="project.description" 
+        :status="project.status"
+        :home="project.home" 
+        :opened="currentMarkerId === project.id"
+        :clickable="project.id !== -1"
+        @set-marker="openMarker"/>
     </GMapMarker>
   </GMapMap>
   <p>{{ center }}</p>
+  <p>{{ currentMarkerId }}</p>
+  <p>{{ projects }}</p>
 </template>
 
 <style scoped> 
---
+++ client/components/LocationResource/LocationResourcePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps(["title", "description"]);
+</script>
+
+<template>
+<p>Title: {{ props.title }}</p>
+<p>Project Descriptions: {{ $props.description }}</p>
+<p>List of posts: Empty</p>
+<p>Actionables: Empty</p>
+</template>
+
+<style scoped>
+</style>
