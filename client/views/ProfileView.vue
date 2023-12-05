91b86a40b6857561710321096aaa01ee4bb29fff
Lucy Kim <lvcyklm02@gmail.com>
2023-12-04 23:13:01 -0500

    profile view

--- client/views/ProfileView.vue
+++ client/views/ProfileView.vue
@@ -2,9 +2,12 @@
 import router from "@/router";
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 
 const { currentUsername } = storeToRefs(useUserStore());
 const { logoutUser, deleteUser } = useUserStore();
+
+let view = ref("posts");
 
 async function goToSettings() {
     void router.push({ name: "Settings" });
@@ -14,8 +17,30 @@ async function goToSettings() {
 
 <template>
     <main class="column">
-        <h1>Profile for {{ currentUsername }}</h1>
-        <button class="pure-button pure-button-primary" @click="goToSettings">Settings</button>
+        <div class="header">
+            <div class="pfp"></div>
+            <h1>Profile for {{ currentUsername }}</h1>
+
+            <button class="pure-button pure-button-primary" @click="goToSettings">Settings</button>
+        </div>
+        <div class="pf-bar">
+            <ul>
+                <li>Posts</li>
+                <li>Opinions</li>
+                <li>Events</li>
+                <li>Saved</li>
+            </ul>
+        </div>
+
 
     </main>
 </template>
+
+<style>
+.pfp {
+    height: 200px;
+    width: 200px;
+    position: absolute;
+    border-radius: 50%;
+}
+</style>
