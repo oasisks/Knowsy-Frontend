0a551999194d78c636a2b762ea7728e49c5565bd
Lucy Kim <lvcyklm02@gmail.com>
2023-12-05 21:52:18 -0500

    changed default to login

--- client/views/LoginView.vue
+++ client/views/LoginView.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import LoginOrRegister from '../components/Login/LoginOrRegister.vue';
-// import LoginForm from "@/components/Login/LoginForm.vue";
-// import RegisterForm from "@/components/Login/RegisterForm.vue";
 </script>
 
 <template>
   <main class="column">
     <LoginOrRegister />
-    <!-- <LoginForm />
-    <RegisterForm /> -->
   </main>
 </template>
