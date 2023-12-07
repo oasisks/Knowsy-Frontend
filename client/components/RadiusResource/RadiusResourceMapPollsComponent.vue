b5ad0e8a79647287345fea1d73d8eed189cec93d
Julia Camacho <juliac123@gmail.com>
2023-12-07 15:27:43 -0500

    update announcement page

--- client/components/RadiusResource/RadiusResourceMapPollsComponent.vue
+++ client/components/RadiusResource/RadiusResourceMapPollsComponent.vue
@@ -4,9 +4,12 @@ import { storeToRefs } from "pinia";
 import { fetchy } from "../../utils/fetchy";
 import { ObjectId } from "mongodb";
 import { onBeforeMount, ref } from "vue";
+import SmallMap from "@/components/Map/SmallMap.vue";
+import Poll from "@/components/Poll/Poll.vue";
 
 const props = defineProps(["resourceId"]);
 const resource = ref<Record<string, string>>();
+const resourceCoords = ref({ lng: 0, lat: 0 });
 const loaded = ref(false);
 const emit = defineEmits(["editPost", "refreshPosts"]);
 const { currentUsername } = storeToRefs(useUserStore());
@@ -15,6 +18,8 @@ async function getResourceFromId(_id: ObjectId) {
   let resource;
   try {
     resource = await fetchy(`/api/radiusResources/${_id}`, "GET");
+    resourceCoords.value.lng = resource.location.coordinates[0];
+    resourceCoords.value.lat = resource.location.coordinates[1];
   } catch (_) {
     console.log("catching", _);
   }
@@ -29,26 +34,7 @@ onBeforeMount(async () => {
 
 <template>
   <section v-if="loaded && resource !== null">
-    <div class="mb-10">
-      <div class="mb-5 justify-items-start">
-        <h1 class="text-xl font-semibold text-sky-500">Announcement</h1>
-        <h2 class="text-3xl font-bold">{{ resource?.name }}</h2>
-      </div>
-      <div class="mb-3">
-        <p class="text-lg font-semibold">Critical dates:</p>
-        <article v-for="date in resource?.criticalDates" :key="date">
-          <!-- <p class="text-lg">{{ JSON.parse(date).info }} on {{ JSON.parse(date).time }}</p> -->
-          <!-- <p class="text-lg">{{ JSON.parse(date.toString()) }}</p> -->
-          <!-- TODO: reformat this -->
-          <p class="text-lg">{{ date }}</p>
-        </article>
-      </div>
-      <div>
-        <p class="text-lg font-semibold">Description:</p>
-        <p class="text-lg">{{ resource?.content }}</p>
-      </div>
-    </div>
-
-    <p>[Insert Actionables here]</p>
+    <SmallMap :project-coords="resourceCoords" />
+    <Poll :id="$props.resourceId" />
   </section>
 </template>
--- client/views/AnnouncementView.vue
+++ client/views/AnnouncementView.vue
@@ -2,6 +2,7 @@
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
 import RadiusResourceOverviewComponent from "@/components/RadiusResource/RadiusResourceOverviewComponent.vue";
+import RadiusResourceMapPollsComponent from "@/components/RadiusResource/RadiusResourceMapPollsComponent.vue";
 import OpinionList from "@/components/Opinion/OpinionList.vue";
 
 const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
@@ -10,8 +11,15 @@ const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
 <template>
   <main class="sm:mx-12 sm:my-10 mx-4 my-3">
     <section v-if="isLoggedIn">
-      <RadiusResourceOverviewComponent v-bind:resourceId="$route.params._id" />
-      <OpinionList v-bind:rootId="$route.params._id" :profile="false" />
+      <div class="grid grid-cols-3">
+        <div class="col-span-2">
+          <RadiusResourceOverviewComponent v-bind:resourceId="$route.params._id" />
+          <OpinionList v-bind:rootId="$route.params._id" :profile="false" />
+        </div>
+        <div>
+          <RadiusResourceMapPollsComponent v-bind:resourceId="$route.params._id" />
+        </div>
+      </div>
     </section>
     <h1 v-else>Please login!</h1>
   </main>
