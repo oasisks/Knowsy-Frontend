d76ab2de932b267ed15a801570b783d5cfa01a73
Hao Zhu <oasisks2@gmail.com>
2023-12-13 15:53:21 -0500

    Fixed the bug where it brings the poll to the top if selected

--- client/components/Poll/PollList.vue
+++ client/components/Poll/PollList.vue
@@ -1,18 +1,34 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, onUpdated, ref } from 'vue';
 import PollComponent from './PollComponent.vue';
 
 
 const props = defineProps(["polls"]);
+const finished = ref(false);
 const emits = defineEmits(["refresh"]);
 
+function sortPolls() {
+    // we only want to sort by the prompt
+    props.polls.sort((a: any, b: any) => {
+        return a.prompt.localeCompare(b.prompt);
+    })
+}
+
 onMounted(async () => {
+    // on mount we want to sort the polls 
+    console.log(props.polls);
+    sortPolls();
+    finished.value = true;
+})
+
+onUpdated(() => {
+    sortPolls();
 })
 
 </script>
 
 <template>
-    <div class="w-full overflow-auto h-64 flex flex-col gap-2">
+    <div v-if="finished" class="w-full overflow-auto h-64 flex flex-col gap-2">
         <div v-for="(poll, i) in props.polls" :key="poll.id">
             <PollComponent :poll="props.polls[i]" @refresh="emits(`refresh`)"/>
         </div>
